cca540738398b01b246f1427d2156ea16237a707
mrzamin <142754418+mrzamin@users.noreply.github.com>
2023-12-16 13:14:32 -0500

    Add functions for basic math operators: add, subtract, multiply, divide

---
+++ main.js
@@ -0,0 +1,17 @@
+//Create functions for basic math operators.
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
